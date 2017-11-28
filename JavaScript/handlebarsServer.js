abd5d8d93d3da60fa4019acb7ee11bcb1fc993aa
Michael Wolfe <michael.wolfe101@gmail.com>
2017-11-28 17:59:08 -0500

    handlebars server setup

---
+++ JavaScript/handlebarsServer.js
@@ -0,0 +1,16 @@
+// Dependencies
+var express = require("express");
+var path = require("path");
+var exphbs = require("express-handlebars");
+
+// Create an instance of the express app.
+var app = express();
+
+// Specify the port.
+var port = 3000;
+
+// Set Handlebars as the default templating engine.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.listen(port);
