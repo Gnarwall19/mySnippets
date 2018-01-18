102d299390a7925f4e13181a5ce771ddbf951567
Michael Wolfe <Michael@Michaels-MacBook-Pro.local>
2018-01-17 22:20:30 -0500

    added to randomNumber

--- JavaScript/randomNumber.js
+++ JavaScript/randomNumber.js
@@ -1,5 +1,13 @@
 function randomRange(min, max) {
 
-  return Math.floor(Math.random() * (max - min +1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+
+}
+
+// OR
+
+function randomRange2(min, max) {
+
+  return Math.floor(Math.random() * max) + min;
 
 }
